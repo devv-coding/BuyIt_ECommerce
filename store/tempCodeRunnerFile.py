4da911ae941cd06ea6c99abcf3fa9fda5079ea8f
Anjali Dubey <37217269+devv-coding@users.noreply.github.com>
2024-12-30 19:01:42 +0530

    Add files via upload

---
+++ store/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+class Product(models.Model):
+	name = models.CharField(max_length=200)
+	price = models.FloatField()
+	digital = models.BooleanField(default=False,null=True, blank=True)
+	image = models.ImageField(upload_to='images/', null=True, blank=True)
